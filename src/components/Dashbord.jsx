f5adea43bb36e04ae70ef2d039b7075c5dddf5c8
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2024-12-05 17:12:04 +0200

    test3

--- src/components/Dashbord.jsx
+++ src/components/Dashbord.jsx
@@ -19,14 +19,11 @@ export async function dashbordLoder() {
       mood: moodData[index],
     }));
 
-    await fetch(
-      "https://www.harmony-headlines-backend.onrender.com/initial-mood",
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ allData }),
-      }
-    );
+    await fetch("https://harmony-headlines-backend.onrender.com/initial-mood", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ allData }),
+    });
 
     console.log(allData);
     return allData;
